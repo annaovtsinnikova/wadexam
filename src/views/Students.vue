b25ac8c69b35019f8fc0b1453bb34e6352a22c93
Anna Ovtsinnikova <ovtsinnikova.a@g,ail.com>
2024-01-19 15:10:56 +0200

    task 4

--- src/views/Students.vue
+++ src/views/Students.vue
@@ -1,48 +1,3 @@
-<!-- <template>
-  <div>
-<h3> Students grades </h3>
-
-
-<div class="container">
-    
-</div>
-</div>
-</template>
-
-<script>
-export default {
-  name: "Students",
-  data() {
-    return {
-      grades: [],
-    };
-  },
-  methods: {
-    fetchRecords() {
-      fetch(`http://localhost:3000/api/grades`)
-        .then((response) => response.json())
-        .then((data) => (this.grades = data))
-        .catch((err) => console.log(err.message));
-  },
-  },
-  mounted() {
-    this.fetchRecords();
-    console.log("mounted");
-  } 
-};
-</script>
-
-<style scoped>
-h1 {
-  font-size: 20px;
-}
-
-table {
-  color: deepskyblue;
-}
-
-</style> -->
-
 
 <template>
   <div>
@@ -67,13 +22,18 @@ table {
             <td>{{ grade.hw1 }}</td>
             <td>{{ grade.hw2 }}</td>
             <td>{{ grade.exam }}</td>
-            <td :style="{ backgroundColor: getBackgroundColor(grade.final, grade.exam) }">
+            <td :style="{ backgroundColor: getBackgroundColor(grade.final, grade.exam) }" @click="showGradeJustification(grade)">
               {{ grade.final }}
             </td>
           </tr>
         </tbody>
       </table>
     </div>
+
+    <div v-if="showJustification" class="justification-box">
+      <p> {{ justificationText }}</p>
+    </div>
+
   </div>
 </template>
 
@@ -83,6 +43,8 @@ export default {
   data() {
     return {
       grades: [],
+      showJustification: false,
+      justificationText: "",
     };
   },
   methods: {
@@ -97,8 +59,28 @@ export default {
     },
 
     getBackgroundColor(finalGrade, examScore) {
-      return finalGrade < 21 || examScore < 21 ? "red" : "green";
+      return finalGrade < 51 || examScore < 21 ? "red" : "green";
+    },
+
+    showGradeJustification(grade) {
+      this.justificationText = this.calculateJustification(grade);
+      this.showJustification = true;
+    },
+
+    calculateJustification(grade) {
+      if (grade.final < 51 && grade.exam < 21) {
+        return "You failed because you have to get to get 21 or more points for the exam and 51 point or more in total";
+
+      } else if (grade.final < 51) {
+        return "You failed because you have to get 51 points or more in total";
+
+      } else if (grade.exam < 21) {
+        return "You failed because you have to get to get 21 or more points for the exam";
+      } else {
+        return "You passed bacause you got over 20 points for the exam and over 50 points in total";
+      }
     }
+
   },
   mounted() {
     this.fetchRecords();
@@ -134,5 +116,15 @@ th {
 
 td {
   background-color: lightseagreen;
+}
+
+.justification-box {
+  margin-top: 20px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  background-color: steelblue;
+  color: azure;
+  font-size: 20px;
+  
 }
 </style>
